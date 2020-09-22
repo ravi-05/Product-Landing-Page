dc6a7c02872f84e8f7e3474e4fe2c84647a9e1a1
ravi-05 <ravivarmara22@gmail.com>
2020-09-22 19:10:33 +0530

    initial commit

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 
     $(".navbar-nav a").on('click' ,function(event){
         if(this.hash !== ""){
+            
             event.preventDefault();
 
             var hash = this.hash;
